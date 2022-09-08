0b5715c3a557f5692718604c6f6d4754446e936b
SOUVIIK-HUB <68889300+SOUVIIK-HUB@users.noreply.github.com>
2022-09-08 23:07:36 +0530

    Delete app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-option = st.selectbox(
-'What chart do you want to see?',
-('Pie Chart & Bar Chart', 'Histogram', 'Box Plot & Column Chart'))
-
-st.write('You selected:', option)
